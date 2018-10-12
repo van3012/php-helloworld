1762cf149d6af33bcc00cf70f2202784e4e57a6e
root <van3012@gmail.com>
2018-10-12 17:23:11 +0800

    new change

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World! Hello OpenShift!</p>'; ?> 
+ <?php echo '<p>Hello World! Hello OpenShift! Hello new change!</p>'; ?> 
  </body>
 </html>
+
